49c042e0363ec4fc8e1eeb1638ff522daf810ae7
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-05-04 20:55:38 -0300

    feat: implement getNextFuriaMatch function to fetch upcoming FURIA matches

---
+++ src/lib/furiaData.ts
@@ -0,0 +1,28 @@
+export async function getNextFuriaMatch() {
+  const token = process.env.PANDASCORE_API_KEY;
+
+  const res = await fetch(
+    "https://api.pandascore.co/csgo/matches?filter[number_of_games]=3&filter[opponent_id]=furia&filter[future]=true&sort=&page=1&per_page=10",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!res.ok) throw new Error("Erro ao buscar partidas da FURIA");
+
+  const data = await res.json();
+
+  if (data.length === 0) return null;
+
+  const match = data[0];
+
+  return {
+    opponent: match.opponents.find((o: any) => o.opponent.name !== "FURIA")
+      ?.opponent.name,
+    beginsAt: match.begin_at,
+    league: match.league?.name,
+    serie: match.serie?.full_name,
+  };
+}
